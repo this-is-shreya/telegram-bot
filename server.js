9d5b2a64b9aef91e2b0ae8e732dc7ea05e695b3d
major jerk <shreyasrivastava28@gmail.com>
2022-02-21 16:02:25 +0530

    added port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const app = require("express")()
 const {Telegraf,session, Scenes:{Stage, WizardScene}} = require("telegraf")
 const dotenv = require("dotenv")
+const { application } = require("express")
 dotenv.config({path:"./config.env"})
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
@@ -8,3 +10,4 @@ bot.start(ctx=>{
 })
 
 bot.launch()
+app.listen(process.env.PORT || 3000)
